69431ad4f1f4c6a188e225c049065cf2a8ed1f4c
LuizMVGBarbosa <lmvagliengo@gmail.com>
2023-02-04 15:34:32 -0300

    lessons 01,02 and 3 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+const number = Math.floor(Math.random() * 1000);
 
+const repeatString = function(string,num) {
+   let repeated ='';
+   if(num>=0){
+   let i=0
+   while(i<num){
+     repeated += string
+     i++
+   }
+   return repeated;}
+   else{
+    repeated="ERROR"
+    return repeated;
+   }
 };
+repeatString('hey',3);
+repeatString('hey',10);
+repeatString('hey',1);
+repeatString('hey',0);
+repeatString('hey',-1);
+repeatString('hey',number);
+repeatString('',10);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed="";
+    let count =string.length;
+    for(let i=0;i<=count;i++){
+      let letter="";
+       letter = string.substr(count-i,1);
+      reversed += letter;
+       }
+      return reversed;
 };
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc!');
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
