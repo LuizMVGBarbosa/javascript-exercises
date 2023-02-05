f6b02d0a3f3b27cb54649c544d11297f0f4018df
LuizMVGBarbosa <lmvagliengo@gmail.com>
2023-02-05 20:30:07 -0300

    i made it with a nested loop yeah!!!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    let preSelected = array
+    for(let i=0;i<=args.length;i++){
+      for(const selected of array){
+          if(arguments[i]===selected){
+              let index =preSelected.indexOf(selected)
+              preSelected.splice(index,1);
+          }
 
+      }
+    }
+    return preSelected;
 };
-
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
