9699db372e8904b2e243dadb5907ce55861e20d2
LuizMVGBarbosa <lmvagliengo@gmail.com>
2023-02-06 19:07:42 -0300

    All exercises made!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(number1,number2) {
+    let sumResult =0;
+    if (number1<0||number2<0||  typeof number1 != 'number'||typeof number2 != 'number'){
+        return "ERROR"
+    }
+     for(let i=0;i<=number1||i<=number2;i++){
+    let counted = 0+i;
+    sumResult += counted;
 };
+  return sumResult;};
+    
+    
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Ftemperature) {
+  let ctemperature = (Ftemperature-32)*0.555555556;
+  return Math.round(ctemperature*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Ctemperature) {
+  let ftemperature =(Ctemperature*1.8)+32;
+  return Math.round(ftemperature*10)/10;
 };
 
 // Do not edit below this line
