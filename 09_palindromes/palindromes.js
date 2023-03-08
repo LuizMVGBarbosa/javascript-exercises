1cf9c74bebc84260e26317cecec7f77a209fa5e2
LuizMVGBarbosa <lmvagliengo@gmail.com>
2023-03-08 19:07:40 -0300

    Exercise made!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reversed='';
+    let count =string.length;
+    for(let i=0;i<=count;i++){
+      let letter='';
+       letter = string.substr(count-i,1);
+      reversed += letter;
+       }
+    let phrase = string.toLowerCase()
+                       .split("")
+                       .filter(letter=>{
+                         if(letter!="!"&&letter!=" "&&letter!=","&&letter!="."){
+                            return true
+                         }});
+    let phraseReversed = reversed.toLowerCase()
+                             .split("")
+                             .filter(letter=>{
+                                if(letter!="!"&&letter!=" "&&letter!=","&&letter!="."){
+                                   return true
+                                }});
+        if(JSON.stringify(phrase)=== JSON.stringify(phraseReversed )){
+            return true;
+        }
+        else{
+            return false;
+        }
+    
 };
 
 // Do not edit below this line
