ef941e00a1b26577ab20f223fcc12f6c97f6ba68
LuizMVGBarbosa <lmvagliengo@gmail.com>
2023-03-08 20:54:03 -0300

    Exercise made!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+  let fiboNumber =[];
+  for(let i = 0;i<number;i++){
+    if(fiboNumber.length<2){
+    fiboNumber.push(1)}
+    else{
+        let lastfibo = fiboNumber[fiboNumber.length-1]+fiboNumber[fiboNumber.length-2];
+        fiboNumber.push(lastfibo);}
+    }
+return fiboNumber[fiboNumber.length-1];
 };
 
 // Do not edit below this line
