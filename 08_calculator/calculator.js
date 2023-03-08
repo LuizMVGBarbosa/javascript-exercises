e739c6a412bf1f3378e313c0cf1ef0da0188a447
LuizMVGBarbosa <lmvagliengo@gmail.com>
2023-03-08 17:22:25 -0300

    Exercise made!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
+const add = function(a,b) {
+	let result = a+b;
+  return result
+};
+
+const subtract = function(a,b) {
+  let result = a-b;
+  return result
 	
 };
 
-const subtract = function() {
+const sum = function(numbers) {
+  let array = numbers;
+  return array.reduce((total,number)=> total+number,0)
+  };
+
+const multiply = function(numbers) {
+  let result = 0;
+  numbers.forEach(number => {
+    if(result==0){
+      result=number
+    }
+   else{
+    result*=number
+   }
+})
+  return result;
 	
 };
 
-const sum = function() {
-	
+const power = function(...numbers) {
+  let array =[...numbers];
+  let result=0;
+  let expo =0;
+  array.forEach(number => {
+  if(result==0 ){
+    result=number;
+     
+  }
+  else{
+    expo=result;
+    for(let i=1;i<number;i++){
+      result*=expo
+    }
+  }
+    })
+    return result;
+    
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result=1;
+  for(let i=0;i<number;i++){
+    result*=(number-i)
+  }
+	return result;
 };
 
 // Do not edit below this line
