6fd1d6dfd58011316914d62b366a9e155611f8d7
LuizMVGBarbosa <lmvagliengo@gmail.com>
2023-03-10 10:55:49 -0300

    exercise made!

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(object) {
+    let titles =[]
+    const bookShelf = object.filter(book =>{
+        if(book.title!="")
+        titles.push(book.title)
+    })
+   return  titles;
 };
 
 // Do not edit below this line
