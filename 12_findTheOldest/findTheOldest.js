fb1433343aede81f354865c7bc6afd139faacce5
LuizMVGBarbosa <lmvagliengo@gmail.com>
2023-03-10 13:53:58 -0300

    Exercise made!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(person) {
+    let oldest ;
+    const currentYear = new Date().getFullYear();
+      const ordered = person.sort((a,b)=>{
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = currentYear;
+          }
+          if (!b.yearOfDeath) {
+            b.yearOfDeath = currentYear;
+          }
+       const lastGuy = a.yearOfDeath - a.yearOfBirth;
+       const nextGuy = b.yearOfDeath - b.yearOfBirth;
+       if(lastGuy>nextGuy){
+        return -1;
+      }
+        else{
+          return 1;
+        }
+   }
+      )
+     oldest = ordered[0];
+    return oldest};
 // Do not edit below this line
 module.exports = findTheOldest;
